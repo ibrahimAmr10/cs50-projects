508ae6626db3aef24523123e55e678654c697ee1
ibrahimAmr10 <magictech205@gmail.com>
2026-02-10 14:01:34 +0200

    Add files via upload

---
+++ pset2-arrays/caesar.c
@@ -0,0 +1,48 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, string argv[])
+{
+    // لازم يكون في key واحد بس
+    if (argc != 2)
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+    for (int i = 0; argv[1][i] != '\0'; i++)
+    {
+        if (!isdigit(argv[1][i]))
+        {
+            printf("Usage: ./caesar key\n");
+            return 1;
+        }
+    }
+    int k = atoi(argv[1]);
+
+    string plaintext = get_string("plaintext: ");
+    printf("ciphertext: ");
+
+    for (int i = 0; i < strlen(plaintext); i++)
+    {
+        char c = plaintext[i];
+
+        if (isupper(c))
+        {
+            printf("%c", (c - 'A' + k) % 26 + 'A');
+        }
+        else if (islower(c))
+        {
+            printf("%c", (c - 'a' + k) % 26 + 'a');
+        }
+        else
+        {
+            printf("%c", c);
+        }
+    }
+
+    printf("\n");
+    return 0;
+}
